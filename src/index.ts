4eb355ec4ba3e82110241b30102c2ce41a2b5b79
Michael Poythress <michaelpoythress@Michaels-MacBook-Pro-FF.local>
2024-01-29 23:44:20 -0800

    namespaced portals

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,54 @@
  *
  * See examples/workbooks/FullExample.ts for a full, working example of a Workbook.
  */
-import { Workbook, } from '@flatfile/configure'
-import {
-    portals,
-    sheets
-} from './sheets';
 
-export const MyWorkbook = new Workbook( {
-    name : 'Entry Workbook',
-    namespace : 'entry-workbook',
-    portals,
-    sheets,
+import { Workbook, } from '@flatfile/configure'
+// import {portals, sheets} from './sheets';
+
+import { AvSpendPortal, AvSpendSheet } from "./sheets/spend"
+import { AvFuelPortal, AvFuelSheet } from "./sheets/fuel"
+import { AvEnergyPortal, AvEnergySheet } from './sheets/energy';
+import { AvTravelPortal, AvTravelSheet } from './sheets/travel';
+
+// export const MyWorkbook = new Workbook( {
+//     name : 'Entry Workbook',
+//     namespace : 'entry-workbook',
+//     portals,
+//     sheets,
+// })
+
+export const spendWorkbook = new Workbook({
+
+    name: "Spend Workbook",
+    namespace: "spendNamespace",
+    portals: [AvSpendPortal],
+    sheets: { AvSpendSheet }
+    
+})
+
+export const fuelWorkbook = new Workbook({
+
+    name: "Fuel Workbook",
+    namespace: "fuelNamespace",
+    portals: [AvFuelPortal],
+    sheets: { AvFuelSheet }
+
+})
+
+export const energyWorkbook = new Workbook({
+
+    name: "Energy Workbook",
+    namespace: "energyNamespace",
+    portals: [AvEnergyPortal],
+    sheets: { AvEnergySheet }
+
+})
+
+export const travelWorkbook = new Workbook({
+
+    name: "Travel Workbook",
+    namespace: "travelNamespace",
+    portals: [AvTravelPortal],
+    sheets: { AvTravelSheet }
+
 })
--- src/listener.ts
+++ src/listener.ts
@@ -1,11 +1,27 @@
-import { MyWorkbook } from "./index.js";
+// import { MyWorkbook } from "./index.js";
+import { spendWorkbook, fuelWorkbook, energyWorkbook, travelWorkbook } from "./index"
 import { dxpConfigure } from "@flatfile/plugin-dxp-configure";
 import { FlatfileListener } from "@flatfile/listener";
 import { xlsxExtractorPlugin } from "@flatfile/plugin-xlsx-extractor";
 
 export default (listener: FlatfileListener) => {
-    
-    listener.use(xlsxExtractorPlugin());
 
-    listener.use(dxpConfigure(MyWorkbook));
+  listener.namespace(['*:spend'], (spend) =>
+    spend.use(dxpConfigure(spendWorkbook))
+  )
+
+  listener.namespace(['*:fuel'], (fuel) => 
+    fuel.use(dxpConfigure(fuelWorkbook))
+  )
+
+  listener.namespace(['*:energy'], (energy) =>
+    energy.use(dxpConfigure(energyWorkbook))
+  )
+
+  listener.namespace(['*:travel'], (travel) =>
+    travel.use(dxpConfigure(travelWorkbook))
+  )
+  
+  listener.use(xlsxExtractorPlugin())
+
   };
