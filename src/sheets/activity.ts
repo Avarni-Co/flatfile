17b6fa608d94183d816bb18e85be628b7fd2898b
Jalanie Dimacaling <jalanie.dimacaling@avarni.co>
2024-05-27 19:38:07 +0800

    revert sub org activity field for now

--- src/sheets/activity.ts
+++ src/sheets/activity.ts
@@ -3,7 +3,7 @@ import { countriesv2 } from '../constants';
 
 import { AnyField, Portal, Sheet } from '@flatfile/configure';
 import { FlatfileRecord, FlatfileSession } from '@flatfile/hooks';
-import { computeSubOrg, validateGhgCategory, validateInputUnit } from '../utils';
+import { validateGhgCategory, validateInputUnit } from '../utils';
 
 export type ActivityEntryFlatfileRecordKeys = keyof ActivityEntryFlatfileRecord;
 export interface ActivityEntryFlatfileRecord {
@@ -35,15 +35,13 @@ const activityBaseFields = {
     countries: countriesv2,
     errorMessage: 'Could not validate this country. Please check the guide for a list of valid country names.',
   })({ required: false, label: 'Country' }),
-  subOrganisation: AvTextField({ required: false, label: 'Sub-Organisation' }),
 } as { [index in ActivityEntryFlatfileRecordKeys]: AnyField };
 
 export const AvActivitySheet = new Sheet('Activity Sheet', activityBaseFields, {
   allowCustomFields: true,
-  recordCompute: (record: FlatfileRecord, session: FlatfileSession, logger) => {
+  recordCompute: (record: FlatfileRecord, session: FlatfileSession) => {
     validateInputUnit(record, session);
     validateGhgCategory(record, session);
-    computeSubOrg(record, session, logger);
   },
 });
 
